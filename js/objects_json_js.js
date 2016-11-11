d8bd587405dce9dec114ee1c7c7ec39a77142c4c
hmt3design <hmt3design@gmail.com>
2016-11-11 16:53:13 -0600

    Commit to Codeup-Web-Exercises 111116

--- js/objects_json_js.js
+++ js/objects_json_js.js
@@ -11,59 +11,43 @@
 // var books = [todo];
 
 
-var books = {
-    author: [
-        {
-            authorFirst: "Tom",
-            authorLast: "Clancy"
-        }, {
-            title: "The Hunt for Red October"
-        },
-    ]
-}
-console.log(books)
-
-// {
-//     authorFirst:"J.K.",
-//         authorLast
-// :
-//     "Rowling",
-//         title
-// :
-//     "Harry Potter and the Deathly Hallows"
-// }
-// ,
-// {
-//     authorFirst:"Clive",
-//         authorLast
-// :
-//     "Cussler",
-//         title
-// :
-//     "Raise the Titanic!"
-// }
-// ,
-// {
-//     authorFirst:"Robert",
-//         authorLast
-// :
-//     "Heinlein",
-//         title
-// :
-//     "The Moon is a Harsh Mistress"
-// }
-// ,
-// {
-//     authorFirst:"Rick",
-//         authorLast
-// :
-//     "Riordan",
-//         title
-// :
-//     "The Lost Hero"
-// }
-// ]
-// }
+var books = [
+    {
+        "title": "The Hunt for Red October",
+        "author": {
+            "authorFirst": "Tom",
+            "authorLast": "Clancy"
+        }
+    },
+    {
+        "title": "Harry Potter and the Deathly Hallows",
+        "author": {
+            "authorFirst": "J.K.",
+            "authorLast": "Rowling"
+        }
+    },
+    {
+        "title": "Raise the Titanic!",
+        "author": {
+            "authorFirst": "Clive",
+            "authorLast": "Cussler"
+        }
+    },
+    {
+        "title": "The Moon is a Harsh Mistress",
+        "author": {
+            "authorFirst": "Robert A.",
+            "authorLast": "Heinlein"
+        }
+    },
+    {
+        "title": "The Lost Hero",
+        "author": {
+            "authorFirst": "Rick",
+            "authorLast": "Riordan"
+        }
+    }
+]
 
 // log out the books array
 console.log(books);
@@ -71,8 +55,13 @@ console.log(books);
 // todo:
 // Loop through the array of books using .forEach and print out the specified information about each one.
 // start loop here
-console.log("Book #" + todo);
-console.log("Title: " + todo);
-console.log("Author: " + todo);
+books.forEach(function (book, index) {
+var author = book.author.authorFirst + " " + book.author.authorLast;
+console.log("Book #: " + (index+1));
+console.log("Title: " + book.title);
+console.log("Author: " + author);
 console.log("---");
-// end loop here
+})();
+
+//end loop here
+
