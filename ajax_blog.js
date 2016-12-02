70cb96734fddf6c342c810fbad6d20cba6d22eff
hmt3design <hmt3design@gmail.com>
2016-12-02 16:35:38 -0600

    Commit to Codeup-Web-Exercises 120216 - AJAX blog

--- ajax_blog.js
+++ ajax_blog.js
@@ -8,8 +8,20 @@ var populate = function() {
     var mainBlog = [];
     blogContent.done(function(data) {
         data.forEach(function (object) {
-            mainBlog += "<h2>" + object.title + "</h2><br>"
-                    + "<p>" + object.content + "</p>"
+
+            mainBlog += '<div class="media">' + '<br>' +
+                '<div class="media-left">' + '<br>' +
+                    '<img class="media-object" src="' + object.img + '"> </div>' +
+                    '<div class="media-body">' +
+                    '<h3 class="media-heading">' + object.title + '</h3><br>' +
+                    '<p>' +  object.date + '</p><br>' +
+                        '<p>' + object.content + '</p>' + '<br>' +
+                    '</div>' + '<br>' +
+            '</div>'
+            console.log(object.img);
+            // mainBlog += "<h2>" + object.title + "</h2><br>"
+            //         + "<p>" + object.content + "</p>"
+            //         + "<img src='" + object.img + ">";
         });
         $("#posts").html(mainBlog);
     });
@@ -27,3 +39,20 @@ $("#reload").click(function () {
     $.get("data/blog.json");
     populate();
 });
+
+$( function() {
+    $("#datepicker").datepicker();
+} );
+
+
+// Add a blog post
+// var addPost = function(post){
+//     add post object to global post array
+//     append post data to the page
+// };
+// Remove a blog post
+// var removePost = function(index){
+//     find post in array by index
+//     splice post out of the array
+//     remove post element from page
+// };
