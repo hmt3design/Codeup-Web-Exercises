68cbcdb423bc8edc62eb8a35b1c643796fa5441f
hmt3design <hmt3design@gmail.com>
2016-12-06 10:50:35 -0600

    Commit to Codeup-Web-Exercises 12016 - Weather Map, no GoogleMap functionality

--- js/weather_map.js
+++ js/weather_map.js
@@ -4,6 +4,22 @@
 
 $(document).ready(function () {
     "use strict";
+
+    // Set our map options
+    var mapOptions = {
+        // Set the zoom level
+        zoom: 13,
+
+        // This sets the center of the map at our location
+        center: {
+            lat: 29.42412,
+            lng: -98.493629
+        }
+    };
+
+    // Render the map
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
     var setCityNameFromResponse = function (data) {
         $("#current-city").html(data.city.name);
     };
@@ -12,10 +28,10 @@ $(document).ready(function () {
     var setForecast = document.getElementsByClassName("forecast");
 
     var getMaxTemp = function (data) {
-        return data.temp.max;
+        return (Math.round(data.temp.max));
     };
     var getMinTemp = function (data) {
-        return data.temp.min;
+        return (Math.round(data.temp.min));
     };
     var getHumidity = function (data) {
         return data.humidity;
@@ -26,8 +42,12 @@ $(document).ready(function () {
     var getPressure = function (data) {
         return data.pressure;
     };
-
-
+    var getIcon = function (data) {
+        return data.weather[0].icon;
+    };
+    var getDescription = function (data) {
+        return data.weather[0].description;
+    };
     // get today's date
     // var todaysDate = new Date();
     // var day = getDate();
@@ -38,7 +58,7 @@ $(document).ready(function () {
     // call OpenWeatherMap API
     var openWeatherRequest = $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
         APPID: "b4197e496c4b73186de7934cf48d40d6",
-        q: "San Antonio, TX",
+        id: 4726206,
         units: "imperial",
         cnt: 3
     });
@@ -52,7 +72,9 @@ $(document).ready(function () {
                 minTemp: getMinTemp(object),
                 humidity: getHumidity(object),
                 wind: getWind(object),
-                pressure: getPressure(object)
+                pressure: getPressure(object),
+                icon: getIcon(object),
+                description: getDescription(object)
             };
             weatherArray.push(weatherData);
 
@@ -60,14 +82,15 @@ $(document).ready(function () {
         var mainTable;
         weatherArray.forEach(function (object) {
             mainTable =
-                '<div class="col-md-4">' +
-                '<p><strong>Date</strong></p>' + '<br>' +
-                object.maxTemp + " / " + object.minTemp + '<br>' +
-                '<p><em>Humidity</em></p>' + object.humidity + '<br>' +
-                '<p><em>Wind</em></p>' + object.wind + '<br>' +
-                '<p><em>Barometer</em></p>' + object.pressure +
+                '<div class="col-sm-3">' +
+                '<p><strong>Date</strong></p>' +
+                object.maxTemp + " / " + object.minTemp + '</p>' +
+                '<img src="http://openweathermap.org/img/w/' + object.icon + '.png"><p>' + object.description + '</p>' +
+                '<p><em>Humidity: </em>' + object.humidity + '%</p>' +
+                '<p><em>Wind: </em>' + object.wind + ' mph</p>' +
+                '<p><em>Barometer: </em>' + object.pressure + ' hPa</p>'
                 '</div>'
-
+            console.log(object.icon);
             $('#weather-report').append(mainTable);
         });
     });
